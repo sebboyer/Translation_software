9cd3d50d7b5baaecb3f8c9070c29bd7a3c2d0179
sebboyer <sebastienboyer54@gmail.com>
2016-03-08 16:30:20 -0500

    Add prompt questions

--- edx_to_MOOCdb_piping/full_pipe.py
+++ edx_to_MOOCdb_piping/full_pipe.py
@@ -18,12 +18,49 @@ python full_pipe.py FOLDER/ COURSE_NAME COURSE_PREFIX
 
 # from subprocess import check_output
 import subprocess
-
+import sys
 import argparse
 
 
 
 JSON_TO_CSV_SCRIPT = 'import.openedx.apipe/scripts/transformGivenLogfiles.sh'
+
+
+
+
+
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
 
 
 if __name__ == "__main__":
@@ -44,47 +81,59 @@ if __name__ == "__main__":
     ############  Create environment for log data translation
     ################################################################
 
-    print "********  Creating environment **********"
+    question = 'Do you want to create folder environment for Log transformation ?'
 
-    cmd_queue = []
 
-    ## Create directories
-    LOG_DATA = ''.join([args.folder,args.course_name,'/log_data/'])
-    INT_CSV = ''.join([args.folder,args.course_name,'/intermediary_csv/'])
-    MOOC_CSV = ''.join([args.folder,args.course_name,'/moocdb_csv/'])
+    if query_yes_no(question):
+        print "********  Creating environment **********"
 
-    cmd_queue.append(' '.join(['mkdir',LOG_DATA]))
-    cmd_queue.append(' '.join(['mkdir',INT_CSV]))
-    cmd_queue.append(' '.join(['mkdir',MOOC_CSV]))
+        cmd_queue = []
 
-    ## Move and Unzip Json files
-    LOG_FILES = ''.join([args.folder,args.course_name,'/',args.prefix,'*'])
-    cmd_queue.append(' '.join(['mv',LOG_FILES,LOG_DATA[:-1]]))  # don't include /
-    JSON_FILE = ''.join([LOG_DATA,args.prefix,'___tracking_log.json.gz'])
-    cmd_queue.append(' '.join(['gzip','-d',JSON_FILE]))
+        ## Create directories
+        LOG_DATA = ''.join([args.folder,args.course_name,'/log_data/'])
+        INT_CSV = ''.join([args.folder,args.course_name,'/intermediary_csv/'])
+        MOOC_CSV = ''.join([args.folder,args.course_name,'/moocdb_csv/'])
 
-    # for cmd in cmd_queue:
-    #     subprocess.call(cmd, shell=True)
+        cmd_queue.append(' '.join(['mkdir',LOG_DATA]))
+        cmd_queue.append(' '.join(['mkdir',INT_CSV]))
+        cmd_queue.append(' '.join(['mkdir',MOOC_CSV]))
+
+        ## Move and Unzip Json files
+        LOG_FILES = ''.join([args.folder,args.course_name,'/',args.prefix,'*'])
+        cmd_queue.append(' '.join(['mv',LOG_FILES,LOG_DATA[:-1]]))  # don't include /
+        JSON_FILE = ''.join([LOG_DATA,args.prefix,'___tracking_log.json.gz'])
+        cmd_queue.append(' '.join(['gzip','-d',JSON_FILE]))
+
+        # for cmd in cmd_queue:
+        #     subprocess.call(cmd, shell=True)
 
 
     ################################################################
     ############  Launch APIPE (translation from json to intermediary csv)
     ################################################################
 
-    print "********  Translating from json to csv **********" 
-    cmd_queue = []
+    question = 'Do you want to translate log files into csv ?'
+    
+    if query_yes_no(question):
 
-    JSON_FILE = ''.join([LOG_DATA,args.prefix,'___tracking_log.json'])
-    cmd_queue.append(' '.join(['bash',JSON_TO_CSV_SCRIPT,INT_CSV,JSON_FILE]))
+        print "********  Translating from json to csv **********" 
+        cmd_queue = []
 
-    # for cmd in cmd_queue:
-    #     subprocess.call(cmd, shell=True)
+        JSON_FILE = ''.join([LOG_DATA,args.prefix,'___tracking_log.json'])
+        cmd_queue.append(' '.join(['bash',JSON_TO_CSV_SCRIPT,INT_CSV,JSON_FILE]))
+
+        # for cmd in cmd_queue:
+        #     subprocess.call(cmd, shell=True)
 
     ################################################################
     ############ Launch QPIPE
     ################################################################
 
-    print "********  Transform csv and populate MYSQL Table **********" 
+    question = 'Do you want to create folder environment for Log transformation ?'
+
+    if query_yes_no(question):
+        
+        print "********  Transform csv and populate MYSQL Table **********" 
 
 
 
